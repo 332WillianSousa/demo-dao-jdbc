33b8176506752f5122195eef644cd12ed18acc76
Willian Sousa <332williamsousa@gmail.com>
2026-03-02 19:47:09 -0300

    adicionado um novo obj Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,11 +5,10 @@ import model.entities.Department;
 public class Program {
 
 	public static void main(String[] args) {
-		
 		Department obj = new Department(1, "Books");
-		
+		Department obj2 = new Department(2, "Reportagens");
 		System.out.println(obj.toString());
-		
+		System.out.println(obj2.toString());
 	}
 
 }
